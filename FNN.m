d67f4e8b425d50e3a092856b4633856a4228ae7d
Kunal Jain <jain.kunal35@ymail.com>
2017-03-17 19:32:26 +0530

    Added hidden layer with backpropagation
    Backpropagation applied to get weight updates for hidden layer using gradient descent.

--- FNN.m
+++ FNN.m
@@ -2,44 +2,55 @@ function [Acc] = FNN(X,Y,k)
 
 
 if nargin==0
-X=[1  2; 5 6; 3 4];  
-Y=[3 8; 14 24; 10 15];
-% X=[17.5000  576.2000; 35.9000  598.6000]; 
-% Y=[57.6000  848.0000; 24.8000  905.0000]; 
+%  X=[1  2; 5 6; 3 4;7 -4;34 -20];  
+%  Y=[8 14;3  24; 10 15;14 -17;-62 6];
+X=[17.5000  576.2000; 35.9000  598.6000]; 
+Y=[57.6000  848.0000; 24.8000  905.0000]; 
 k=2;
+
+elseif nargin==2
+k=size(X,1);
+
 end
 
-X=X(1:k,:); 
-Y=Y(1:k,:);
+X=X(1:k,:)/1000; 
+X=[ones(size(X,1),1) X];
+Y=Y(1:k,:)/1000;
 
 ni = size(X,2);
-nh = 2;
+nh = 3;
 no = size(Y,2);
-N=150000;
+N=200000;
 
+% a=zeros(size(X,1),nh);
 Z=zeros(size(Y));
-% y=zeros(length(X),no);
 
-W1=rand(nh,ni);
+
+W1=0.0025*rand(nh,ni);
+Wo=5*rand(no,nh+1);
 % W1=[1.019165966126373,-0.012309363730452;32.093452849245050,-0.503837590655945];
-% Wo=ones(nh,no);
-alpha = 0.01;%0.0000001;
 
-Acc=sum(abs(Y-Z));
+alpha = 0.01;
+
+Acc=sum(abs(Y-Z))/k;
 i=0;
-while (sum(Acc)>.1) % (abs(Y-Z)>((10^-1)*ones(size(Y))))
-    Z = ((X*W1'));%poslin
-%     y = (Z*Wo);
-%     Wo = Wo + alpha*Z*(Y-y);
-    W1 = W1 + alpha*(((Y-Z)')*X);
+while (sum(Acc)>0.01)% (abs(Y-Z)>((10^-1)*ones(size(Y))))
+    z = (X*W1');
+    a = 2*logsig(z)-1;
+    a = [ones(size(a,1),1) a];
+    Z = (a*Wo');%poslin
+    e=(Y-Z);
+    hiddenDelta =  (2*a(:,2:end).*(1-a(:,2:end))).*(e*Wo(:,2:end));%
+    Wo = Wo + alpha*((e')*a)/k;
+    W1 = W1 + alpha*(((hiddenDelta)')*X)/k;
     i=i+1;
     if i>N
         break;
     end
-Acc=sum(abs(Y-Z));
+Acc=sum(abs(Y-Z))/k;
 disp([i, Acc]);
 end
 
-A=[X Y Z];
+A=[X Y Z]*1000;
 
 end
